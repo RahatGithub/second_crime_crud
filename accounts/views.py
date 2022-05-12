bf8f67475370af1fdf2697b0beb7a99b1b3e1753
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2022-05-12 23:57:44 +0600

    basic dashboards are ready

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
-# from .models import Reporter, Investigator
+from .models import Reporter, Investigator
 from django.contrib import messages
 from random import randint
 
@@ -32,35 +32,34 @@ def register(request):
 
 
 def registerAsInvestigator(request):
-    # if request.method == "POST" :
-    #     investigator_id = 'INV_' + chr(randint(65,70)) + str(randint(100000,999999))  # Generating an ID for the investigator
-    #     firstname = request.POST['f_name']
-    #     lastname = request.POST['l_name']
-    #     fullname = firstname + ' ' + lastname
-    #     phone= request.POST['phone']
-    #     email = request.POST['email']
-    #     username = request.POST['username']
-    #     password = request.POST['password']
-    #     confirm_password = request.POST['confirm_password']
-    #     if password == confirm_password : 
-    #         if User.objects.filter(username = username).exists():
-    #             messages.info(request, "Username already taken")
-    #             return redirect("register")
-    #         elif User.objects.filter(email = email).exists():
-    #             messages.info(request, "Email already in use")
-    #             return redirect("register")
-    #         else :
-    #             user = User.objects.create_user(username, email, password, first_name=firstname, last_name=lastname)
-    #             user.save()
-    #             investigator = Investigator(investigator_id=investigator_id, name=fullname, phone=phone, email=email, cases="will be declared soon")
-    #             investigator.save()
-    #             auth.login(request, user)
-    #             return redirect("/main/")
-    #     else :
-    #         messages.info(request, 'Password not matched!')
-    #         return redirect("register")
-    # return render(request, 'accounts/registerAsInvestigator.html')
-    pass
+    if request.method == "POST" :
+        investigator_code = 'INV_' + chr(randint(65,70)) + str(randint(100000,999999))  # Generating a code for the investigator
+        firstname = request.POST['f_name']
+        lastname = request.POST['l_name']
+        fullname = firstname + ' ' + lastname
+        phone= request.POST['phone']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password : 
+            if User.objects.filter(username = username).exists():
+                messages.info(request, "Username already taken")
+                return redirect("register")
+            elif User.objects.filter(email = email).exists():
+                messages.info(request, "Email already in use")
+                return redirect("register")
+            else :
+                user = User.objects.create_user(username, email, password, first_name=firstname, last_name=lastname)
+                user.save()
+                investigator = Investigator(investigator_code=investigator_code, name=fullname, phone=phone, email=email, cases="will be declared soon")
+                investigator.save()
+                auth.login(request, user)
+                return redirect("/main/")
+        else :
+            messages.info(request, 'Password not matched!')
+            return redirect("register")
+    return render(request, 'accounts/registerAsInvestigator.html')
 
 
 
@@ -78,18 +77,17 @@ def login(request):
 
 
 def loginAsInvestigator(request):
-    # if request.method == "POST":
-    #     username = request.POST['username']
-    #     password = request.POST['password']
-    #     user = auth.authenticate(request, username=username, password=password)
-    #     if user:
-    #         auth.login(request, user)
-    #         return redirect("/main/")
-    #     else:    
-    #         messages.info(request, "Please enter valid username and password")       
-    #         return render(request, "accounts/loginAsInvestigator.html")
-    # return render(request, "accounts/loginAsInvestigator.html")
-    pass
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user:
+            auth.login(request, user)
+            return redirect("/main/")
+        else:    
+            messages.info(request, "Please enter valid username and password")       
+            return render(request, "accounts/loginAsInvestigator.html")
+    return render(request, "accounts/loginAsInvestigator.html")
 
 
 def logout(request):
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-# from .models import Case
-# from accounts.models import Reporter
+from .models import Case
+from accounts.models import Reporter
 from django.contrib.auth.models import User, auth
 from random import randint
 from django.http import HttpResponse
@@ -11,8 +11,38 @@ def index(request):
 
 
 def reportCrime(request):
-    pass
+    if request.method == "POST":
+        user = request.user
+        case_code = 'CAS_' + chr(randint(65,70)) + str(randint(100000,999999))  # Generating a code for the case
+        location = request.POST.get('location', '')
+        desc = request.POST.get('desc', '')
+        fullname = user.first_name + ' ' + user.last_name
+        phone = request.POST.get('phone', '')
+        email = user.email 
+        case = Case(case_code=case_code, location=location, desc=desc, status="Pending", reporter_name=fullname, reporter_phone=phone, investigator_code="NA")
+        case.save()
+        reporter = Reporter.objects.filter(email=email)
+        if reporter: 
+            # add this case to the reporter's reports
+            pass
+        else:
+            reporter_code = 'REP_' + chr(randint(65,70)) + str(randint(100000,999999))  # Generating a code for the reporter
+            new_reporter = Reporter(reporter_code=reporter_code, name=fullname, phone=phone, email=email, cases="will be shown later")
+            new_reporter.save()
+    return render(request, 'main/reportCrime.html')
 
 
 def dashboard(request, username):
-    pass
+    user = request.user
+    email = user.email
+    fullname = user.first_name + ' ' + user.last_name
+    cases, userRole = None, None 
+    if Case.objects.filter(reporter_name=fullname).exists(): 
+        cases = Case.objects.filter(reporter_name=fullname)
+        userRole = "reporter" 
+    elif Case.objects.filter(investigator=fullname).exists():
+        cases = Case.objects.filter(investigator=fullname)
+        userRole = "investigator"
+
+    params = {'username':username, 'fullname':fullname, 'cases':cases, 'userRole':userRole}
+    return render(request, 'main/dashboard.html', params)
